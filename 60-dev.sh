a00d5d64c4e41a3fdee09be9e7c7110a30717dda
Will Kimble <wkimble91@protonmail.com>
2023-11-05 15:24:40 -0600

    Create 60-dev.sh

---
+++ 60-dev.sh
@@ -0,0 +1,9 @@
+AddPackage alsa-utils # Advanced Linux Sound Architecture - Utilities
+AddPackage atomicparsley # Reading, parsing and setting metadata in MPEG-4 files (Used by yt-dlp)
+AddPackage autoconf # GNU tool for automatically configuring source code
+AddPackage bc # An arbitrary precision calculator language
+AddPackage binutils # A set of programs to assemble and manipulate binary and object files
+AddPackage bison # The GNU general-purpose parser generator
+AddPackage brasero # CD/DVD mastering tool
+AddPackage btop # A monitor of system resources, bpytop ported to C++
+AddPackage ddcutil # Query and change Linux monitor settings using DDC/CI and USB
