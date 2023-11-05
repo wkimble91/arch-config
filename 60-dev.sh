b9dde49745d4d20a585310a84ec8c17be5b266bd
Will Kimble <wkimble91@protonmail.com>
2023-11-05 16:29:46 -0600

    Update 60-dev.sh

--- 60-dev.sh
+++ 60-dev.sh
@@ -7,3 +7,41 @@ AddPackage bison # The GNU general-purpose parser generator
 AddPackage brasero # CD/DVD mastering tool
 AddPackage btop # A monitor of system resources, bpytop ported to C++
 AddPackage ddcutil # Query and change Linux monitor settings using DDC/CI and USB
+AddPackage dpkg # The Debian Package Manager tools
+AddPackage ffmpegthumbnailer # Lightweight video thumbnailer that can be used by file managers
+AddPackage ffmpegthumbs # FFmpeg-based thumbnail creator for video files
+AddPackage flex # A tool for generating text-scanning programs
+AddPackage gnome-keyring # Stores passwords and encryption keys
+AddPackage gnu-netcat # GNU rewrite of netcat, the network piping application
+AddPackage grep # String search utility
+AddPackage groff # GNU troff text-formatting system
+AddPackage gzip # GNU compression utility
+AddPackage htop # Interactive process viewer
+AddPackage ibus # Next Generation Input Bus for Linux
+AddPackage ibus-anthy # Japanese input method Anthy IMEngine for IBus Framework
+AddPackage jdk17-openjdk # OpenJDK Java 17 development kit
+AddPackage jre-openjdk-headless # OpenJDK Java 21 headless runtime environment
+AddPackage lftp # Sophisticated command line based FTP client
+AddPackage libva # Video Acceleration (VA) API for Linux
+AddPackage lib32-libva # Video Acceleration (VA) API for Linux (32-bit)
+AddPackage libappimage # Reference implementation of the AppImage specification
+AddPackage liblightdm-qt5 # LightDM Qt client library
+AddPackage libtool # A generic library support script
+AddPackage libva-vdpau-driver # VDPAU backend for VA API
+AddPackage lrzip # Multi-threaded compression with rzip/lzma, lzo, and zpaq
+AddPackage mediainfo # Supplies technical and tag information about media files
+AddPackage nodejs # Evented I/O for V8 javascript
+AddPackage perl-rename # Renames multiple files using Perl regular expressions
+AddPackage pkgconf # Package compiler and linker metadata toolkit
+AddPackage repo # The Multiple Git Repository Tool from the Android Open Source Project
+AddPackage sed # GNU stream editor
+AddPackage texinfo # GNU documentation system for on-line information and printed output
+AddPackage vi # Text editor
+AddPackage vim # Highly configurable, improved version of the vi text editor
+AddPackage which # A utility to show the full path of commands
+AddPackage wmctrl # Control your EWMH compliant window manager from command line
+AddPackage yt-dlp # A youtube-dl fork with additional features and fixes
+AddPackage zpaq # Programmable file compressor, library and utilities
+AddPackage wine # A compatibility layer for running Windows programs
+AddPackage wine-mono # Wine's built-in replacement for Microsoft's .NET Framework
+AddPackage winetricks # Script to install various redistributable runtime libraries in Wine
