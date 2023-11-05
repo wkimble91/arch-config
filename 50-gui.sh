de6ea72d08906976a371c6bef3e45dfd9bb2306a
Will Kimble <wkimble91@protonmail.com>
2023-11-05 16:28:56 -0600

    Update 50-gui.sh

--- 50-gui.sh
+++ 50-gui.sh
@@ -1,10 +1,15 @@
 AddPackage ark # GUI archiving KDE software
 AddPackage flameshot # Screenshot utility
 AddPackage grim # Screenshot utility for Wayland
+AddPackage gst-plugins-ugly # Multimedia graph framework
 AddPackage kaccounts-integration # Online account management system and its Plasma integration components
 AddPackage kf5 # Various KDE packages
 AddPackage konsole # CLI interface
+AddPackage lib32-gtk2 # GObject-based multi-platform GUI toolkit (legacy) (32-bit)
+AddPackage lightdm # A lightweight display manager
+AddPackage lightdm-gtk-greeter # GTK+ greeter for LightDM
 AddPackage mako # Lightweight notification daemon for Wayland
+AddPackage partitionmanager # A KDE utility that allows you to manage disks, partitions, and file systems
 AddPackage picom # X compositor that may fix tearing issues
 AddPackage plasma # KDE Plasma Desktop
 AddPackage xorg-server # Xorg X server
