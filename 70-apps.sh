36e303599d1402cedb19ff6025d904064b5a18e7
Will Kimble <wkimble91@protonmail.com>
2023-11-05 16:30:12 -0600

    Update 70-apps.sh

--- 70-apps.sh
+++ 70-apps.sh
@@ -1,2 +1,37 @@
+  GNU nano 7.2                                                                                                   70-apps.sh                                                                                                              
 AddPackage audacious # Simple music player
 AddPackage audacity # Sound editor
+AddPackage baka-mplayer
+AddPackage chromium
+AddPackage deepin-calculator
+AddPackage deluge
+AddPackage deluge-gtk
+AddPackage discord
+AddPackage dolphin
+AddPackage dolphin-plugins
+AddPackage filezilla
+AddPackage firefox
+AddPackage flatpak
+AddPackage gimp
+AddPackage gparted
+AddPackage handbrake
+AddPackage inkscape
+AddPackage kakasi
+AddPackage krenamekf5
+AddPackage leafpad
+AddPackage libreoffice-fresh
+AddPackage lutris
+AddPackage mpv
+AddPackage navidrome
+AddPackage obsidian
+AddPackage openrgb
+AddPackage openshot
+AddPackage qbittorrent
+AddPackage rmlint-shredder
+AddPackage steam
+AddPackage telegram-desktop
+AddPackage torbrowser-launcher
+AddPackage virtualbox
+AddPackage virtualbox-guest-iso
+AddPackage virtualbox-guest-utils
+AddPackage vlc
