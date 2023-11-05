ac7b6ef9cece2182153450c7c1c51a525871abec
Will Kimble <wkimble91@protonmail.com>
2023-11-05 15:23:42 -0600

    Create 50-gui.sh

---
+++ 50-gui.sh
@@ -0,0 +1,15 @@
+AddPackage ark # GUI archiving KDE software
+AddPackage flameshot # Screenshot utility
+AddPackage grim # Screenshot utility for Wayland
+AddPackage kaccounts-integration # Online account management system and its Plasma integration components
+AddPackage kf5 # Various KDE packages
+AddPackage konsole # CLI interface
+AddPackage mako # Lightweight notification daemon for Wayland
+AddPackage picom # X compositor that may fix tearing issues
+AddPackage plasma # KDE Plasma Desktop
+AddPackage xorg-server # Xorg X server
+AddPackage xorg-server-xephyr # A nested X server that runs as an X application
+AddPackage xorg-xdpyinfo # Display information utility for X
+AddPackage xorg-xinit # Allows user to manually start an Xorg display server
+AddPackage xorg-xinput # Small commandline tool to configure devices
+AddPackage xorg-xwayland # run X clients under wayland
