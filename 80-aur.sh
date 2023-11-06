a20bdc95d3d2ef42f57c2d02bf321ede7b7004da
Will Kimble <wkimble91@protonmail.com>
2023-11-06 12:08:22 -0600

    Update 80-aur.sh

--- 80-aur.sh
+++ 80-aur.sh
@@ -1,41 +1,44 @@
-AddPackage aconfmgr-git
-AddPackage android-sdk-platform-tools
-AddPackage anki
-AddPackage autofs
-AddPackage dualsensectl-git # Dualsense Controller drivers
-AddPackage feishin-appimage
-AddPackage ffmt-bin # FFXIV Modding Tool
-AddPackage freetube-bin
-AddPackage fsearch
-AddPackage gconf
-AddPackage google-chrome
-AddPackage heroic-games-launcher-bin
-AddPackage koala-bin
-AddPackage komodo-edit
-AddPackage lib32-ncurses5-compat-libs
-AddPackage mecab
-AddPackage mecab-ipadic
-AddPackage mons #Dual monitor support
-AddPackage mp3tag
-AddPackage openrgb
-AddPackage otf-takao
-AddPackage paru-bin
-AddPackage pocket-casts-linux-bin
-AddPackage popsicle
-AddPackage postman-bin
-AddPackage proton-ge-custom-bin
-AddPackage protontricks
-AddPackage protonvpn
-AddPackage protonvpn-cli
-AddPackage protonvpn-gui
-AddPackage python-proton-client
-AddPackage python-protonvpn-nm-lib
-AddPackage python-vdf
-AddPackage responsively
-AddPackage snapd
-AddPackage ventoy-bin
-AddPackage visual-studio-code-bin
-AddPackage xclicker
-AddPackage yay
-AddPackage youtube-dl
-AddPackage zpaq
+IgnorePackage --foreign aconfmgr-git # A configuration manager for Arch Linux
+IgnorePackage --foreign android-sdk-platform-tools # Platform-Tools for Google Android SDK (adb and fastboot)
+IgnorePackage --foreign anki # Helps you remember facts (like words/phrases in a foreign language) efficiently
+IgnorePackage --foreign aurutils # helper tools for the arch user repository
+IgnorePackage --foreign autofs # A kernel-based automounter for Linux
+IgnorePackage --foreign dualsensectl-git # Tool for controlling Sony PlayStation 5 DualSense controller on Linux
+IgnorePackage --foreign feishin-appimage # A modern self-hosted music player.
+IgnorePackage --foreign ffmt-bin # FFXIV_Modding_Tool (FFMT for short) is a crossplatform commandline interface for Final Fantasy XIV Modding.
+IgnorePackage --foreign freetube-bin # An open source desktop YouTube player built with privacy in mind.
+IgnorePackage --foreign fsearch # A fast graphical file search utility.
+IgnorePackage --foreign gkraken # GUI that allows to control cooling of NZXT Kraken X (X42, X52, X62 or X72) pumps from Linux
+IgnorePackage --foreign google-chrome # The popular web browser by Google (Stable Channel)
+IgnorePackage --foreign heroic-games-launcher-bin # An Open source Launcher for Epic, Amazon and GOG Games
+IgnorePackage --foreign koala-bin # Koala is a GUI application for LESS, Sass and CoffeeScript compilation, to help web developers use these tools more effici>
+IgnorePackage --foreign komodo-edit # Free & Open-Source counterpart of ActiveState Komodo IDE - XUL based
+IgnorePackage --foreign lib32-ncurses5-compat-libs # System V Release 4.0 curses emulation library (32-bit), ABI 5
+IgnorePackage --foreign mecab-ipadic # IPA dictionary for mecab.
+IgnorePackage --foreign minecraft-launcher # Official Minecraft Launcher
+IgnorePackage --foreign mons # POSIX Shell script to quickly manage 2-monitors display.
+IgnorePackage --foreign mp3tag # The universal tag editor
+IgnorePackage --foreign openrgb # Open source RGB lighting control that doesn't depend on manufacturer software.
+IgnorePackage --foreign otf-takao # Japanese outline fonts based on IPA Fonts (otf-ipafont)
+IgnorePackage --foreign paru-bin # Feature packed AUR helper
+IgnorePackage --foreign pocket-casts-linux-bin # Electron wrapper around the Pocket Casts web app with support for MPRIS (media controls)
+IgnorePackage --foreign popsicle # Linux utility for flashing multiple USB devices in parallel, written in Rust
+IgnorePackage --foreign postman-bin # Build, test, and document your APIs faster
+IgnorePackage --foreign proton-ge-custom-bin # A fancy custom distribution of Valves Proton with various patches
+IgnorePackage --foreign protontricks # A simple wrapper that does winetricks things for Proton enabled games.
+IgnorePackage --foreign protonvpn # Official ProtonVPN metapackage that installs protonvpn-gui and protonvpn-cli, maintained by the ProtonVPN team.
+IgnorePackage --foreign responsively # A modified browser built using Electron that helps in responsive web development
+IgnorePackage --foreign snapd # Service and tools for management of snap packages.
+IgnorePackage --foreign ttf-cinecaption # A cinematic Japanese font
+IgnorePackage --foreign ttf-kochi-substitute # High quality Japanese TrueType fonts
+IgnorePackage --foreign ttf-ms-fonts # Core TTF Fonts from Microsoft
+IgnorePackage --foreign ttf-opensans
+IgnorePackage --foreign ttf-quicksand-variable # A sans serif typeface designed by Andrew Paglinawan using geometric shapes (variable)
+IgnorePackage --foreign ventoy-bin # A new bootable USB solution
+IgnorePackage --foreign visual-studio-code-bin # Visual Studio Code (vscode): Editor for building and debugging modern web and cloud applications (official bi>
+IgnorePackage --foreign wd719x-firmware # Driver for Western Digital WD7193, WD7197 and WD7296 SCSI cards
+IgnorePackage --foreign xboxdrv # Userspace gamepad driver for Linux
+IgnorePackage --foreign xclicker # A blazing fast gui autoclicker for linux
+IgnorePackage --foreign yay # Yet another yogurt. Pacman wrapper and AUR helper written in go.
+IgnorePackage --foreign youtube-dl # A command-line program to download videos from YouTube.com and a few more sites
+IgnorePackage --foreign zpaq # Programmable file compressor, library and utilities. Based on the PAQ compression algorithm
