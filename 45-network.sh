47a9a124a0c21ca66e7ea19235fedd3341ac8631
Will Kimble <wkimble91@protonmail.com>
2023-11-06 12:06:00 -0600

    Update 45-network.sh

--- 45-network.sh
+++ 45-network.sh
@@ -1,11 +1,11 @@
-AddPackage iwd # Internet Wireless Daemon
+ddPackage iwd # Internet Wireless Daemon
 AddPackage libnm # NetworkManager client library
 AddPackage lib32-libnm # NetworkManager client library (32-bit)
 AddPackage mkinitcpio-nfs-utils # ipconfig and nfsmount tools for NFS root support in mkinitcpio
 AddPackage network-manager-applet # Applet for managing network connections
 AddPackage networkmanager # Network connection manager and user applications
 AddPackage networkmanager-openvpn # NetworkManager VPN plugin for OpenVPN
-AddPacakge networkmanager-qt5 # Qt wrapper for NetworkManager API
+AddPackage networkmanager-qt5 # Qt wrapper for NetworkManager API
 AddPackage nfs-utils # Support programs for Network File Systems
 AddPackage nm-connection-editor # NetworkManager GUI connection editor and widgets
 AddPackage nmap # Utility for network discovery and security auditing
