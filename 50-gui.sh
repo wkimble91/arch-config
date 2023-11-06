19d76e00c080364157b02b8071a8714f2146c02d
Will Kimble <wkimble91@protonmail.com>
2023-11-06 12:06:20 -0600

    Update 50-gui.sh

--- 50-gui.sh
+++ 50-gui.sh
@@ -1,17 +1,36 @@
 AddPackage ark # GUI archiving KDE software
+AddPackage breeze-plymouth # Plymouth theme for the Breeze visual style for the Plasma Desktop
+AddPackage evince # Document viewer (PDF, PostScript, XPS, djvu, dvi, tiff, cbr, cbz, cb7, cbt) (Used for print previews)
 AddPackage flameshot # Screenshot utility
 AddPackage grim # Screenshot utility for Wayland
 AddPackage gst-plugins-ugly # Multimedia graph framework
 AddPackage kaccounts-integration # Online account management system and its Plasma integration components
-AddPackage kf5 # Various KDE packages
+AddPackage kapidox # Frameworks API Documentation Tools
+AddPackage kcalendarcore5 # The KDE calendar access library
+AddPackage kcontacts5 # Address book API for KDE
+AddPackage kdav5 # A DAV protocol implemention with KJobs
+AddPackage kdegraphics-thumbnailers # Thumbnailers for various graphics file formats
+AddPackage kdenlive # A non-linear video editor for Linux using the MLT video framework
+AddPackage kdesdk-thumbnailers # Plugins for the thumbnailing system
+AddPackage kdoctools5 # Documentation generation from docbook
+AddPackage kfind # Find Files/Folders
+AddPackage kimageformats5 # Image format plugins for Qt5
+AddPackage kinit # Process launcher to speed up launching KDE applications
+AddPackage kplotting5 # Lightweight plotting framework
+AddPackage krename # A very powerful batch file renamer
 AddPackage konsole # CLI interface
 AddPackage lib32-gtk2 # GObject-based multi-platform GUI toolkit (legacy) (32-bit)
 AddPackage lightdm # A lightweight display manager
 AddPackage lightdm-gtk-greeter # GTK+ greeter for LightDM
 AddPackage mako # Lightweight notification daemon for Wayland
+AddPackage oxygen-icons # The Oxygen Icon Theme
 AddPackage partitionmanager # A KDE utility that allows you to manage disks, partitions, and file systems
 AddPackage picom # X compositor that may fix tearing issues
-AddPackage plasma # KDE Plasma Desktop
+AddPackage plasma-meta # Meta package to install KDE Plasma
+AddPackage plasma-sdk # Applications useful for Plasma development
+AddPackage plasma-wayland-protocols # Plasma Specific Protocols for Wayland
+AddPackage plasma-wayland-session # Plasma Wayland session
+AddPackage plymouth-kcm # KCM to manage the Plymouth (Boot) theme
 AddPackage xorg-server # Xorg X server
 AddPackage xorg-server-xephyr # A nested X server that runs as an X application
 AddPackage xorg-xdpyinfo # Display information utility for X
