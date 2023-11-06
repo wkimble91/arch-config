e5b6b99ea78c2330807b6cd5cb071d8ffe1b3f75
Will Kimble <wkimble91@protonmail.com>
2023-11-06 12:07:57 -0600

    Update 70-apps.sh

--- 70-apps.sh
+++ 70-apps.sh
@@ -1,36 +1,35 @@
-AddPackage audacious # Simple music player
-AddPackage audacity # Sound editor
-AddPackage baka-mplayer
-AddPackage chromium
-AddPackage deepin-calculator
-AddPackage deluge
-AddPackage deluge-gtk
-AddPackage discord
-AddPackage dolphin
-AddPackage dolphin-plugins
-AddPackage filezilla
-AddPackage firefox
-AddPackage flatpak
-AddPackage gimp
-AddPackage gparted
-AddPackage handbrake
-AddPackage inkscape
-AddPackage kakasi
-AddPackage krenamekf5
-AddPackage leafpad
-AddPackage libreoffice-fresh
-AddPackage lutris
-AddPackage mpv
-AddPackage navidrome
-AddPackage obsidian
-AddPackage openrgb
-AddPackage openshot
-AddPackage qbittorrent
-AddPackage rmlint-shredder
-AddPackage steam
-AddPackage telegram-desktop
-AddPackage torbrowser-launcher
-AddPackage virtualbox
-AddPackage virtualbox-guest-iso
-AddPackage virtualbox-guest-utils
-AddPackage vlc
+AddPackage audacious # Lightweight, advanced audio player focused on audio quality
+AddPackage audacity # A program that lets you manipulate digital audio waveforms
+AddPackage baka-mplayer # A free and open source, cross-platform, libmpv based multimedia player. Qt5 build.
+AddPackage chromium # A web browser built for speed, simplicity, and security
+AddPackage deepin-calculator # An easy to use calculator for ordinary users
+AddPackage deluge # BitTorrent client with multiple user interfaces in a client/server model
+AddPackage deluge-gtk # GTK UI for Deluge
+AddPackage discord # All-in-one voice and text chat for gamers
+AddPackage dolphin # KDE File Manager
+AddPackage dolphin-plugins # Extra Dolphin plugins
+AddPackage filezilla # Fast and reliable FTP, FTPS and SFTP client
+AddPackage firefox # Standalone web browser from mozilla.org
+AddPackage flatpak # Linux application sandboxing and distribution framework (formerly xdg-app)
+AddPackage flatpak-kcm # Flatpak Permissions Management KCM
+AddPackage gimp # GNU Image Manipulation Program
+AddPackage gparted # A Partition Magic clone, frontend to GNU Parted
+AddPackage handbrake # Multithreaded video transcoder
+AddPackage inkscape # Professional vector graphics editor
+AddPackage kakasi # Convert Kanji characters to Hiragana, Katakana or Romaji
+AddPackage leafpad # A notepad clone for GTK+ 2.0
+AddPackage libreoffice-fresh # LibreOffice branch which contains new features and program enhancements
+AddPackage lutris # Open Gaming Platform
+AddPackage mpv # a free, open source, and cross-platform media player
+AddPackage navidrome # A web-based music collection server and streamer
+AddPackage obsidian # A powerful knowledge base that works on top of a local folder of plain text Markdown files
+AddPackage openshot # An award-winning free and open-source video editor
+AddPackage qbittorrent # An advanced BitTorrent client programmed in C++, based on Qt toolkit and libtorrent-rasterbar
+AddPackage rmlint-shredder # Graphical user interface for rmlint
+AddPackage steam # Valve's digital software delivery system
+AddPackage telegram-desktop # Official Telegram Desktop client
+AddPackage torbrowser-launcher # Securely and easily download, verify, install, and launch Tor Browser in Linux
+AddPackage virtualbox # Powerful x86 virtualization for enterprise as well as home use
+AddPackage virtualbox-guest-iso # The official VirtualBox Guest Additions ISO image
+AddPackage virtualbox-guest-utils # VirtualBox Guest userspace utilities
+AddPackage vlc # Multi-platform MPEG, VCD/DVD, and DivX player
