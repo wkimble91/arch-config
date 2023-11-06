a7c9a919fb13ad59cdbbb7d59e477aeb0f6571bc
Will Kimble <wkimble91@protonmail.com>
2023-11-06 12:07:14 -0600

    Update 55-font.sh

--- 55-font.sh
+++ 55-font.sh
@@ -9,25 +9,43 @@ AddPackage noto-fonts-extra # Google Noto TTF fonts and additional variants
 AddPackage opendesktop-fonts # Chinese TrueType Fonts
 AddPackage otf-font-awesome # Iconic font designed for Bootstrap
 AddPackage otf-ipafont # Japanese outline fonts by Information-technology Promotion Agency, Japan (IPA)
+AddPackage powerline-fonts # patched fonts for powerline
 AddPackage ttf-baekmuk # Korean fonts
 AddPackage ttf-caladea # A serif font family metric-compatible with Cambria font family
 AddPackage ttf-carlito # Google's Carlito font
-AddPackage ttf-cinecaption # A cinematic Japanese font
 AddPackage ttf-dejavu # Font family based on the Bitstream Vera Fonts with a wider range of characters
 AddPackage ttf-droid # General-purpose fonts released by Google as part of Android
-AddPackage ttf-hanazono # A free Japanese kanji font, which contains about 78,685 characters (and 2 SPACEs) defined in ISO/IEC 10646 standard / the Unicode standard
+AddPackage ttf-hanazono # A free Japanese kanji font, which contains about 78,685 characters (and 2 SPACEs) defined in ISO/IEC 10646 standard / the Unicode>
 AddPackage ttf-ibm-plex # IBM Plex Mono, Sans, and Serif
-AddPackage ttf-kochi-substitute # High quality Japanese TrueType fonts
 AddPackage ttf-linux-libertine # Serif (Libertine) and Sans Serif (Biolinum) OpenType fonts with large Unicode coverage
 AddPackage ttf-linux-libertine-g # Graphite port of Linux Libertine and Linux Biolinum fonts
-AddPackage ttf-ms-fonts # Core TTF Fonts from Microsoft
 AddPackage ttf-mplus-nerd # Patched font MPlus from nerd fonts library
-AddPackage ttf-opensans # Sans-serif typeface commissioned by Google
-AddPackage ttf-quicksand-variable # A sans serif typeface designed by Andrew Paglinawan using geometric shapes
 AddPackage ttf-sazanami # Full CJK and Latin-1 TTF font combining Sazanami-Mincho and Hanazono glyphs
 
+CopyFile /usr/local/share/fonts/.uuid '' will users
+
+CopyFile /usr/local/share/fonts/k/KanjiStrokeOrders_v4.004.ttf
+CopyFile /usr/local/share/fonts/k/Koruri_Bold.ttf
+CopyFile /usr/local/share/fonts/k/Koruri_Extrabold.ttf
+CopyFile /usr/local/share/fonts/k/Koruri_Light.ttf
+CopyFile /usr/local/share/fonts/k/Koruri_Regular.ttf
+CopyFile /usr/local/share/fonts/k/Koruri_Semibold.ttf
+CopyFile /usr/local/share/fonts/k/komatuna.ttf
+CopyFile /usr/local/share/fonts/k/komatuna_p.ttf
+CopyFile /usr/local/share/fonts/m/MMCedar.ttf
+CopyFile /usr/local/share/fonts/m/MMCedar_P.ttf
+CopyFile /usr/local/share/fonts/m/meguri.ttf
+CopyFile /usr/local/share/fonts/m/meguri_p.ttf
+CopyFile /usr/local/share/fonts/m/mona.ttf
+CopyFile /usr/local/share/fonts/m/monapo.ttf
+CopyFile /usr/local/share/fonts/u/umeplus_cl_gothic.ttf
+CopyFile /usr/local/share/fonts/u/umeplus_clp_gothic.ttf
+CopyFile /usr/local/share/fonts/u/umeplus_gothic.ttf
+CopyFile /usr/local/share/fonts/u/umeplus_p_gothic.ttf
+CopyFile /usr/local/share/fonts/v/VL_Gothic_regular.ttf
+
 CreateLink /etc/fonts/conf.d/10-hinting-slight.conf /usr/share/fontconfig/conf.default/10-hinting-slight.conf
-CreateLink /etc/fonts/conf.d/10-scale-bitmap-fonts.conf /usr/share/fontconfig/conf.default/10-hinting-slight.conf
+CreateLink /etc/fonts/conf.d/10-scale-bitmap-fonts.conf /usr/share/fontconfig/conf.default/10-scale-bitmap-fonts.conf
 CreateLink /etc/fonts/conf.d/10-sub-pixel-rgb.conf /usr/share/fontconfig/conf.default/10-sub-pixel-rgb.conf
 CreateLink /etc/fonts/conf.d/10-yes-antialias.conf /usr/share/fontconfig/conf.default/10-yes-antialias.conf
 CreateLink /etc/fonts/conf.d/11-lcdfilter-default.conf /usr/share/fontconfig/conf.default/11-lcdfilter-default.conf
